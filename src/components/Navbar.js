1009b25cfd6f5e9942303ae1af298a469f8019c5
Nishchal-007 <letscollabtogether10@gmail.com>
2021-04-08 19:48:06 +0530

    Navbar Updates

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/contact">Contact</a>
+        </div>
+    );
+};
+
+export default Navbar;
